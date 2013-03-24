4c10abd3c18fa3a2fefa88cd9413a00c0cf196cf
Matt Watkajtys <watkajtys@gmail.com>
2013-03-24 18:47:30 -0400

    Added Action for New/Create. Created form partial.
    Also updated index file to display all images. Updated and Created form partial.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,17 @@
 class ImagesController < ApplicationController
 
    def index
+      @images = Image.all
    end
-   
+
+   def new
+      @image = Image.new
+   end
+
+   def create
+      @image = Image.new(params[:image])
+      if @image.save
+         redirect_to images_path
+      end 
+   end
 end
