4afda5e76ea714bf4b6bc33ab6bb50d098e703d7
Matt Watkajtys <watkajtys@gmail.com>
2013-03-24 17:51:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photostream::Application.config.secret_token = '74c8c550a623687e721af23b54d89bedca1372882c4cd09300496b206ec6588fab6028978d18b1926c3510613f9e088b47035ff75b56e0df0f0fe19dfd3e3162'
